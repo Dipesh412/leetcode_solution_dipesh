4e609a9f4bc21067f6d8d78738212a0649cbabed
Dynamic617 <112308318+Dynamic617@users.noreply.github.com>
2022-11-17 18:54:06 +0530

    Create 3.Remove Duplicates from Sorted Array.cpp

---
+++ C++/3.Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+        
+        while(right < nums.size()) {
+            if(nums[left] != nums[right]) {
+                left++;
+                nums[left] = nums[right];
+            }
+            right++;
+        }
+        
+        return left+1;
+    }
+};
